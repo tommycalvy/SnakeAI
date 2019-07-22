506a868077c95d798a95866479e83379011d8e67
Tommy Calvy <thomaslcalvy@gmail.com>
2019-07-22 15:17:11 -0400

    Working Game!!!

--- static/app.js
+++ static/app.js
@@ -1,51 +1,94 @@
 var canvas;
 var unitLength;
-var numOfUnits = 32;
-var gameRatio = 0.8;
-var alive = false;
+var numOfUnits;
+var gameRatio;
 var posX;
 var posY;
-var snakeX;
-var snakeY;
-var direction = 0;
+var snakeX = [];
+var snakeY = [];
+var direction;
+var appleX;
+var appleY;
+var count;
+var border;
+var valid;
 
 
 function setup() {
-  canvas = createCanvas(displayWidth, displayHeight);
-  unitLength = (displayHeight / numOfUnits) * gameRatio;
+  numOfUnits = 32;
+  gameRatio = 0.9;
+  posX = numOfUnits / 2;
+  posY = numOfUnits / 2;
+  snakeX.push(posX);
+  snakeY.push(posY);
+  unitLength = (windowHeight / numOfUnits) * gameRatio;
+  direction = 0;
+  border = 1;
+  appleX = Math.floor(Math.random() * (numOfUnits - border) + border);
+  appleY = Math.floor(Math.random() * (numOfUnits - border) + border);
+  count = 0;
+  canvas = createCanvas(windowWidth, windowHeight);
   background(0);
   stroke(100);
-  for (var i = 1; i <= numOfUnits; i++) {
+  for (var i = border; i < numOfUnits + border; i++) {
     line(unitLength, i * unitLength, numOfUnits * unitLength, i * unitLength);
     line(i * unitLength, unitLength, i * unitLength, numOfUnits * unitLength);
   }
-  frameRate(6);
+  fill('rgb(100, 0, 0)');
+  rect(appleX * unitLength, appleY * unitLength, unitLength, unitLength);
+  frameRate(10);
 }
 
 function draw() {
-  if (direction = 0) {
-    posY++;
-  } else if (direction = 1) {
+  if (direction == 0) {
+    posY--;
+  } else if (direction == 1) {
     posX++;
-  } else if (direction = 2) {
-    poxY--;
-  } else if (direction = 3) {
+  } else if (direction == 2) {
+    posY++;
+  } else if (direction == 3) {
     posX--;
   }
   if (isDead()) {
+    for (var i = 0; i < snakeX.length; i++) {
+      fill(0);
+      rect(snakeX[i] * unitLength, snakeY[i] * unitLength, unitLength, unitLength);
+    }
     snakeX = [];
     snakeY = [];
     posX = numOfUnits / 2;
     posY = numOfUnits / 2;
+    count = 0;
+  } else {
+    fill('rgb(0, 100, 0)');
+    rect(posX * unitLength, posY * unitLength, unitLength, unitLength);
+    if (posX == appleX && posY == appleY) {
+      count++;
+      console.log(count);
+      valid = false;
+      while (!valid) {
+        appleX = Math.floor(Math.random() * (numOfUnits - border) + border);
+        appleY = Math.floor(Math.random() * (numOfUnits - border) + border);
+        valid = true;
+        for (var i = 0; i < snakeX.length; i++) {
+          if (appleX == snakeX[i] && appleY == snakeY[i]) {
+            valid = false;
+            break;
+          }
+        }
+      }
+
+      fill('rgb(100, 0, 0)');
+      rect(appleX * unitLength, appleY * unitLength, unitLength, unitLength);
+    } else {
+      fill(0);
+      rect(snakeX.shift() * unitLength, snakeY.shift() * unitLength, unitLength, unitLength);
+    }
   }
   snakeX.push(posX);
   snakeY.push(posY);
-  fill('rgb(0, 100, 0)');
-  for (var i = 0; i < snakeX.length; i++) {
-    rect(snakeX[i] * unitLength, snakeY[i] * unitLength, unitLength, unitLength);
-  }
 }
-
+/*
 function keyPressed() {
   if (keyCode == 87) {
     direction = 0;
@@ -57,9 +100,21 @@ function keyPressed() {
     direction = 3;
   }
 }
+*/
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    direction = 0;
+  } else if (keyCode === RIGHT_ARROW) {
+    direction = 1;
+  } else if (keyCode === DOWN_ARROW) {
+    direction = 2;
+  } else if (keyCode === LEFT_ARROW) {
+    direction = 3;
+  }
+}
 
 function isDead() {
-  if (posX < 1 || posX > numOfUnits - 2 || posY < 1 || posY > numOfUnits - 2) {
+  if (posX < border || posX > numOfUnits - border || posY < border || posY > numOfUnits - border) {
     return true;
   } else {
     for (var i = 0; i < snakeX.length; i++) {
